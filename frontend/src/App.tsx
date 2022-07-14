207b9d5cb1ea18e68981b92cbae8c24b8710adb5
westorres9 <wester.torres9@gmail.com>
2022-07-14 08:19:37 -0300

    projeto limpo, add bootstrap

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Vendas</h1>
+  )
+}
+
+export default App
