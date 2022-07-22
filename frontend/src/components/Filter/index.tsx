c713677fb24a964dfa6a3765819b5cc3d156b3c4
westorres9 <wester.torres9@gmail.com>
2022-07-22 10:26:06 -0300

    filter-component

--- frontend/src/components/Filter/index.tsx
+++ frontend/src/components/Filter/index.tsx
@@ -1,9 +1,28 @@
 import './styles.css';
+import 'flatpickr/dist/themes/material_green.css';
+import FlatPicker from 'react-flatpickr';
+import flatpickrLib from 'flatpickr';
+import {Portuguese} from 'flatpickr/dist/l10n/pt'
+
+
+flatpickrLib.localize(Portuguese);
 
 const Filter = () => {
+    const onChangeDate = (dates:Date[]) => {
+        console.log(dates);
+    }
     return (
         <div className="filter-container base-card">
-            <h1>filter</h1>
+            <FlatPicker
+            options={{
+                mode: 'range',
+                dateFormat: 'd/m/Y',
+                showMonths: 2
+            }}
+            className="filter-input"
+            onChange={onChangeDate} 
+            placeholder="Selecione um periodo"
+            />
         </div>
     );
 }
